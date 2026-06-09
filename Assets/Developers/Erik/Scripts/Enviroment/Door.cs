fd32eae8fc7b70f7d6097053916d0de80992382e
AndresPedroza-Games <andresepedrozaruni@gmail.com>
2026-06-09 16:56:24 +0200

    Added new patrols points and enemies & added door can be open without key

--- Assets/Developers/Erik/Scripts/Enviroment/Door.cs
+++ Assets/Developers/Erik/Scripts/Enviroment/Door.cs
@@ -2,12 +2,19 @@ using UnityEngine;
 
 public class Door : MonoBehaviour, IInteractable {
     [SerializeField] private GameObject requiredKey;
+    [SerializeField] private bool _DontNeedKey;
 
 
     public void Interact(Player player) {
-        if (player.Inventory.CurrentKey == null)
+        if (player.Inventory.CurrentKey == null && !_DontNeedKey)
         {
             UIController.uiController.ChangeTextBox("Closed Door");
+            return;
+        }
+
+        if (_DontNeedKey)
+        {
+            OpenDoor();
             return;
         }
 
