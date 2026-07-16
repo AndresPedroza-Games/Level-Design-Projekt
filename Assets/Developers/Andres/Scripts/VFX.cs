0b88f829582be5b6acea55bfe08fbd998b28a85e
AndresPedroza-Games <andresepedrozaruni@gmail.com>
2026-07-16 18:11:43 +0200

    Added light to door

--- Assets/Developers/Andres/Scripts/VFX.cs
+++ Assets/Developers/Andres/Scripts/VFX.cs
@@ -5,14 +5,18 @@ using System.Collections;
 public class VFX : MonoBehaviour
 {
     [SerializeField] private List<ParticleSystem> _BoltParticles = new List<ParticleSystem>();
+    [SerializeField] private GameObject _Light;
 
     [SerializeField] private float _CoolDownLightingBolt = 0.1f;
+    [SerializeField] private float _CoolDownLight = 0.05f;
 
     private bool _CanDropBolt;
+    private bool _LightIsActive;
 
     private void Awake()
     {
         _CanDropBolt = true;
+        _LightIsActive = true;
     }
 
     private void Update()
@@ -21,6 +25,13 @@ public class VFX : MonoBehaviour
         {
             _CanDropBolt = false;
             StartCoroutine(RestartBolt());
+        }
+
+        if (_LightIsActive)
+        {
+            _Light.SetActive(_LightIsActive);
+            _LightIsActive = false;
+            StartCoroutine(StopLight());
         }
     }
 
@@ -44,5 +55,12 @@ public class VFX : MonoBehaviour
         yield return new WaitForSeconds(_CoolDownLightingBolt);
         StartParticle(_BoltParticles);
         _CanDropBolt = true;
+    }
+
+    private IEnumerator StopLight()
+    {
+        yield return new WaitForSeconds(_CoolDownLight);
+        _Light.SetActive(_LightIsActive);
+        _LightIsActive = true;
     }
 }
