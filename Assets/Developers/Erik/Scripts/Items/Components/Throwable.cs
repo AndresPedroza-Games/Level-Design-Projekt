1b12313112430203beef1acc202b59504f3d8b03
Schnglbrgr <erikschneigelberger@web.de>
2026-07-28 17:09:29 +0200

    added enemies chasing throwable objects

--- Assets/Developers/Erik/Scripts/Items/Components/Throwable.cs
+++ Assets/Developers/Erik/Scripts/Items/Components/Throwable.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
 
+
 [RequireComponent(typeof(Rigidbody))]
-public class Throwable : MonoBehaviour, IInteractable ,IThrowable {
+public class Throwable : MonoBehaviour, IInteractable, IThrowable {
+
+	[Header("---Config---")]
+	[SerializeField] private LayerMask distractionLayerMask;
+	[SerializeField] private float distractionRange;
 
 	private Rigidbody rb;
 
@@ -18,7 +23,29 @@ public class Throwable : MonoBehaviour, IInteractable ,IThrowable {
 
 
 	public void Throw(Vector3 dir, float force) {
-	    rb.AddForce(dir * force, ForceMode.Impulse);
-    }
+		rb.AddForce(dir * force, ForceMode.Impulse);
+	}
+
+
+	private void OnCollisionEnter(Collision collision) {
+		TryDistractEnemy();
+	}
+
+
+	private void TryDistractEnemy() {
+		Collider[] hits = Physics.OverlapSphere(transform.position, distractionRange);
+
+		foreach (Collider hit in hits) {
+			if (hit.TryGetComponent(out Enemy enemy)) {
+				enemy.Distract(transform);
+			}
+		}
+	}
+
+
+	private void OnDrawGizmosSelected() {
+		Gizmos.color = Color.blue;
+		Gizmos.DrawWireSphere(transform.position, distractionRange);
+	}
 
 }
